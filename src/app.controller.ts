462f8bba536598f2cb09e258992dfa2edc3c8e3a
Desarrollo <Desarrollo@DESKTOP-F722E5D>
2021-09-05 17:22:41 -0500

    commit ambiente

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,17 +1,30 @@
-import { Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { prototype } from 'events';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
+  @Get(':id/code/:user_code')
+  getHello(
+    @Param('id') user_id:number,
+    @Param('user_code') code:string
+  ): any {
+    console.log("user: " + user_id);
+    console.log("code: " + code);
+
     return this.appService.getHello();
   }
 
-  @Post()
-  postaData():void{
+  @Post(':id')
+  postaData(
+    @Param('id') id:number,
+    @Body() data:any
+  ):string{
+    console.log("body : " , data);
+    
     console.log("test");
+    return "Buenas tardes!."
   }
 }
